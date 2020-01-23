ca5190410bdfdbb579697546c1b1198b8b1ae169
Student <example@example.com>
2020-01-23 15:42:01 -0800

    Add logic for search function

--- Library/Controllers/HomeController.cs
+++ Library/Controllers/HomeController.cs
@@ -1,14 +1,35 @@
 using Microsoft.AspNetCore.Mvc;
+using Library.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
 
 namespace Library.Controllers
 {
     public class HomeController : Controller
     {
 
-      [HttpGet("/")]
-      public ActionResult Index()
+      private readonly LibraryContext _db;
+
+      public HomeController(LibraryContext db)
       {
-        return View();
+        _db = db;
+      }
+      [HttpGet("/")]
+      public ActionResult Index(string Search)
+      {
+        List<Author> model = _db.Authors.ToList();
+        List<Book> modelb = _db.Books.Include(books => books.Author).ToList();
+        // var combine = model.Concat(modelb);
+      if(Search!=null) {
+        model = _db.Authors.Where(author => author.Name.Contains(Search)).ToList();
+        modelb = _db.Books.Where(books => books.Description.Contains(Search)).ToList();
+      
+      }
+      var combine = model.AddRange(modelb);
+      return View(combine);
       }
 
     }
